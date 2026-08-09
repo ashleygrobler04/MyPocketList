c0e7dc6d59cf6e40280054b6c48c038331d09c04
ashleygrobler04 <ashleygrobler04@gmail.com>
2026-08-09 19:01:31 +0200

    Added better dialog handling for alerts and confirmation, replacing fluent ui dialogs that is not too accessible at the time

--- MyPocketList/Program.cs
+++ MyPocketList/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 using MyPocketList;
+using MyPocketList.Service;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents();
+builder.Services.AddScoped<IInfoService, InfoService>();
 
 await builder.Build().RunAsync();
---
+++ MyPocketList/Services/InfoService.cs
@@ -0,0 +1,29 @@
+using Microsoft.JSInterop;
+
+namespace MyPocketList.Service;
+
+public interface IInfoService
+{
+    Task Alert(string message);
+    Task<bool> Confirm(string message);
+}
+
+public class InfoService : IInfoService
+{
+    private readonly IJSRuntime js;
+
+    public InfoService(IJSRuntime js)
+    {
+        this.js = js;
+    }
+
+    public async Task Alert(string message)
+    {
+        await js.InvokeVoidAsync("alert", message);
+    }
+
+    public async Task<bool> Confirm(string message)
+    {
+        return await js.InvokeAsync<bool>("confirm", message);
+    }
+}
